fix outcome mappings, phi and delta computation

examples/main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import numpy as np
 from fst_pso3 import PSO
@@ -41,13 +42,14 @@ if __name__ == "__main__":
     psolist = []
     fuzzilist = []
     for i in range(100):
-        pso = PSO(ackley, 50, 20, [-10] * 2, [10] * 2, num_particles=20)
-        fuzzypso = FuzzyPSO(ackley, 50, 20, [-10] * 2, [10] * 2, num_particles=20)
+        pso = PSO(ackley, 50, sys.float_info.max, [-10] * 2, [10] * 2)
+        fuzzypso = FuzzyPSO(ackley, 50, sys.float_info.max, [-10] * 2, [10] * 2)
+
         result = pso.solve()
         resultfuzzy = fuzzypso.solve()
 
-        psolist.append(result[1])
-        fuzzilist.append(resultfuzzy[1])
+        psolist.append(np.linalg.norm(result[0]))
+        fuzzilist.append(np.linalg.norm(resultfuzzy[0]))
 
     print("PSO")
     print(np.mean(psolist))

fst_pso3/fuzzy.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from typing import Callable, List, Dict, Optional, Tuple
 from dataclasses import dataclass
@@ -18,6 +19,7 @@ class FuzzyThresholds:
 class FuzzyParticle(Particle):
     def __init__(self, dimensions: int, lower_bound: list, upper_bound: list):
         super().__init__(dimensions, lower_bound, upper_bound)
+
         self.phi = 0.0  # Performance change
         self.delta = 0.0  # Distance from personal best
 
@@ -59,6 +61,32 @@ class FuzzyPSO(PSO):
         fuzzy_reasoner.add_rules(rules)
 
         return fuzzy_reasoner
+
+    def phi(
+        self,
+        particle: FuzzyParticle,
+    ) -> float:
+
+        def calculate_right_factor() -> float:
+            min_fw_fn = min(self.estimated_worst_fitness, particle.current_fitness)
+            min_fw_fo = min(self.estimated_worst_fitness, particle.previous_fitness)
+
+            return (min_fw_fn - min_fw_fo) / abs(
+                self.estimated_worst_fitness - self.global_best_fitness
+            )
+
+        max_delta = np.max(self.max_velocity)
+
+        if max_delta == 0:
+            raise ValueError("move_max cannot be zero")
+
+        if particle.step_magnitude == 0:
+            return 0.0
+
+        left = particle.step_magnitude / max_delta
+        right = calculate_right_factor()
+
+        return left * right
 
     def _create_fuzzy_set(self, points: List[List[float]], term: str) -> FuzzySet:
         return FuzzySet(points=points, term=term, high_quality_interpolate=False)
@@ -104,48 +132,68 @@ class FuzzyPSO(PSO):
         return phi_rules + delta_rules
 
     def _get_outcome_mapping(self) -> Dict[str, Dict[str, float]]:
+        LOW_INERTIA = 0.3
+        MEDIUM_INERTIA = 0.5
+        HIGH_INERTIA = 1.0
+
+        LOW_SOCIAL = 1.0
+        MEDIUM_SOCIAL = 2.0
+        HIGH_SOCIAL = 3.0
+
+        LOW_COGNITIVE = 0.1
+        MEDIUM_COGNITIVE = 1.5
+        HIGH_COGNITIVE = 3.0
+
+        LOW_MINSP = 0
+        MEDIUM_MINSP = 0.001
+        HIGH_MINSP = 0.01
+
+        LOW_MAXSP = 0.1
+        MEDIUM_MAXSP = 0.15
+        HIGH_MAXSP = 0.2
+
         return {
             "WORSE": {
-                "INERTIA": 0.1,
-                "SOCIAL": 2.0,
-                "COGNITIVE": 1.5,
-                "MIN_VELOCITY": 0.1,
-                "MAX_VELOCITY": 1.0,
+                "INERTIA": LOW_INERTIA,
+                "SOCIAL": HIGH_SOCIAL,
+                "COGNITIVE": MEDIUM_COGNITIVE,
+                "MIN_VELOCITY": HIGH_MINSP,
+                "MAX_VELOCITY": HIGH_MAXSP,
             },
             "SAME": {
-                "INERTIA": 0.5,
-                "SOCIAL": 1.5,
-                "COGNITIVE": 1.5,
-                "MIN_VELOCITY": 0.05,
-                "MAX_VELOCITY": 0.5,
+                "INERTIA": MEDIUM_INERTIA,
+                "SOCIAL": MEDIUM_SOCIAL,
+                "COGNITIVE": MEDIUM_COGNITIVE,
+                "MIN_VELOCITY": LOW_MINSP,
+                "MAX_VELOCITY": MEDIUM_MAXSP,
             },
             "BETTER": {
-                "INERTIA": 0.9,
-                "SOCIAL": 1.0,
-                "COGNITIVE": 2.0,
-                "MIN_VELOCITY": 0.0,
-                "MAX_VELOCITY": 0.5,
+                "INERTIA": HIGH_INERTIA,
+                "SOCIAL": LOW_SOCIAL,
+                "COGNITIVE": HIGH_COGNITIVE,
+                "MIN_VELOCITY": LOW_MINSP,
+                "MAX_VELOCITY": MEDIUM_MAXSP,
             },
             "DELTA_SAME": {
-                "INERTIA": 0.1,
-                "SOCIAL": 1.5,
-                "COGNITIVE": 1.5,
-                "MIN_VELOCITY": 0.1,
-                "MAX_VELOCITY": 0.3,
+                "INERTIA": LOW_INERTIA,
+                "SOCIAL": MEDIUM_SOCIAL,
+                "COGNITIVE": MEDIUM_COGNITIVE,
+                "MIN_VELOCITY": MEDIUM_MINSP,
+                "MAX_VELOCITY": LOW_MAXSP,
             },
             "DELTA_NEAR": {
-                "INERTIA": 0.5,
-                "SOCIAL": 1.0,
-                "COGNITIVE": 1.5,
-                "MIN_VELOCITY": 0.05,
-                "MAX_VELOCITY": 0.5,
+                "INERTIA": MEDIUM_INERTIA,
+                "SOCIAL": LOW_SOCIAL,
+                "COGNITIVE": MEDIUM_COGNITIVE,
+                "MIN_VELOCITY": MEDIUM_MINSP,
+                "MAX_VELOCITY": MEDIUM_MAXSP,
             },
             "DELTA_FAR": {
-                "INERTIA": 0.1,
-                "SOCIAL": 1.5,
-                "COGNITIVE": 1.5,
-                "MIN_VELOCITY": 0.1,
-                "MAX_VELOCITY": 0.3,
+                "INERTIA": LOW_INERTIA,
+                "SOCIAL": MEDIUM_SOCIAL,
+                "COGNITIVE": MEDIUM_COGNITIVE,
+                "MIN_VELOCITY": MEDIUM_MINSP,
+                "MAX_VELOCITY": LOW_MAXSP,
             },
         }
 
@@ -177,10 +225,8 @@ class FuzzyPSO(PSO):
 
     def update_fuzzy_parameters(self, particle: FuzzyParticle):
         current_fitness = particle.current_fitness
-        particle.phi = (particle.best_fitness - current_fitness) / abs(
-            particle.best_fitness
-        )
-        particle.delta = np.linalg.norm(particle.position - particle.best_position)
+        particle.phi = self.phi(particle)
+        particle.delta = np.linalg.norm(particle.position - self.global_best_position)
 
         self.fuzzy_reasoner.set_variable("PHI", particle.phi)
         self.fuzzy_reasoner.set_variable("DELTA", particle.delta)

fst_pso3/pso.py
@@ -16,7 +16,9 @@ class Particle(object):
         self.velocity = np.zeros(dimensions)
         self.best_position = copy.deepcopy(self.position)
         self.best_fitness = sys.float_info.max
+
         self.current_fitness = sys.float_info.max
+        self.previous_fitness = sys.float_info.max
 
         self.step_magnitude = 0.0
         self.distance_from_best = sys.float_info.max
@@ -42,7 +44,7 @@ class PSO:
         self.dimensions = len(lower_bound)
 
         if num_particles is None:
-            self.num_particles = 2 + int(np.sqrt(self.dimensions))
+            self.num_particles = int(10 + 2 * np.sqrt(self.dimensions))
         else:
             self.num_particles = num_particles
 
@@ -55,6 +57,7 @@ class PSO:
 
         self.global_best_position: Optional[np.array] = None
         self.global_best_fitness = sys.float_info.max
+        self.estimated_worst_fitness = sys.float_info.min
 
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
@@ -135,8 +138,16 @@ class PSO:
             particle.best_fitness = fitness
 
     def update_global_best(self):
+
         updated = False
         for particle in self.particles:
+
+            # Update estimated worst fitness at first iteration
+            if self.iteration == 0:
+                if particle.best_fitness > self.estimated_worst_fitness:
+                    self.estimated_worst_fitness = particle.best_fitness
+
+            # Update global best
             if particle.best_fitness < self.global_best_fitness:
                 self.global_best_position = copy.deepcopy(particle.best_position)
                 self.global_best_fitness = particle.best_fitness
@@ -148,6 +159,7 @@ class PSO:
             self.since_last_improvement = 0
 
     def calculate_fitness(self, particle: Particle) -> float:
+        particle.previous_fitness = particle.current_fitness
         particle.current_fitness = self.fitness(particle.position)
         return particle.current_fitness
 